e56e72d7091cb3d31b9ee44f9857f3b9b86cbc0a
xasmall <61656165+xasmall@users.noreply.github.com>
2022-06-22 16:55:34 +0800

    Update HotTagCache.java

--- src/main/java/life/majiang/community/cache/HotTagCache.java
+++ src/main/java/life/majiang/community/cache/HotTagCache.java
@@ -13,15 +13,23 @@ import java.util.PriorityQueue;
  * Created by codedrinker on 2019/8/2.
  */
 
+
+
+// 实现更新热门榜单的功能
+
 @Component
 @Data
 public class HotTagCache {
     private List<String> hots = new ArrayList<>();
 
+    /**
+    * 参数map<String,Integer> 分别为话题名和热度
+    **/
     public void updateTags(Map<String, Integer> tags) {
         int max = 10;
         PriorityQueue<HotTagDTO> priorityQueue = new PriorityQueue<>(max);
 
+        // 将其添加到优先队列中，如果优先队列数量超过10个，那么比较最小值
         tags.forEach((name, priority) -> {
             HotTagDTO hotTagDTO = new HotTagDTO();
             hotTagDTO.setName(name);
